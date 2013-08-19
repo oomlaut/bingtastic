8b1b094382ac32c4ba789bfa492925eca3536938
Paul Gueller <paul.gueller@7summitsagency.com>
2013-08-18 22:45:49 -0500

    analytics and robots settings

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $prefix = "http://www.bing.com/search?setmkt=en-US&q=";
 		<title>it's bingtastic!</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="robots" content="noindex, nofollow">
 		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 		<link rel="stylesheet" href="styles/main.css">
 	</head>
@@ -181,6 +182,17 @@ $prefix = "http://www.bing.com/search?setmkt=en-US&q=";
 					});
 				});
 			})(jQuery);
+		</script>
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-1575455-10', 'paulgueller.com');
+		  ga('send', 'pageview');
+
 		</script>
 	</body>
 </html>
