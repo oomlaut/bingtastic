dce78d28542eb1baea580087853ecef9f10fde00
Paul Gueller <paul.gueller@7summitsagency.com>
2013-08-21 11:33:02 -0500

    refactor automation form, using element-bound events

--- index.php
+++ index.php
@@ -35,66 +35,77 @@ $prefix = "http://www.bing.com/search?setmkt=en-US&q=";
 		<link rel="stylesheet" href="styles/main.css">
 	</head>
 	<body class="container">
+		<div id="fb-root"></div>
 		<!--[if lt IE 7]>
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 		<![endif]-->
-		<div id="fb-root"></div>
+
 		<hgroup class="row">
 			<h1 class="span8">bingtastic!</h1>
 			<div id="login" class="span3"></div>
 		</hgroup>
+
 		<section class="row">
 
-		<div class="span6">
-			<h2>Get Rewarded for &ldquo;Using&rdquo; <a href="http://go.microsoft.com/?linkid=9778718&rrid=_ad8ea4a6-b009-6b60-1c54-667a72de00e5">bing</a></h2>
-			<p><a href="http://go.microsoft.com/?linkid=9778718&rrid=_ad8ea4a6-b009-6b60-1c54-667a72de00e5">Sign up</a> and start earning points that you can turn into contest entries, Amazon or Starbucks gift cards, Hulu Plus subscription, or Redbox rentals. </p>
-			<h3>&ldquo;But, I Like <abbr title='Does it rhyme with "Moogle"?'>[Other Search Engine]</abbr> Better&hellip;&rdquo;</abbr></h3>
-			<p>That&rsquo;s where <em>bingtastic</em> comes in. Make sure you are logged in to Facebook, then use the fields provided to automate your bing queries.</p>
-			<p>We&rsquo;ll generate as many <?php echo $minwords; ?>-<?php echo $maxwords; ?> word phrases as you need, then open up a new tab next door to help you get those precious, precious points.</p>
-			<h3>Search Daily</h3>
-			<p>Earn from 15 to more than 30 points every day and <a href="http://www.bing.com/rewards/redeem/all">redeem them for one of many great offers</a>, or even donate them to a charity.</p>
-		</div>
+			<div class="span6">
+				<h2>Get Rewarded for &ldquo;Using&rdquo; <a href="http://go.microsoft.com/?linkid=9778718&rrid=_ad8ea4a6-b009-6b60-1c54-667a72de00e5">bing</a></h2>
+				<p><a href="http://go.microsoft.com/?linkid=9778718&rrid=_ad8ea4a6-b009-6b60-1c54-667a72de00e5">Sign up</a> and start earning points that you can turn into contest entries, Amazon or Starbucks gift cards, Hulu Plus subscription, or Redbox rentals. </p>
 
-		<form action="/" method="get" class="form-horizontal span5 offset1 row">
-			<h2>Automate Searches</h2>
-			<fieldset>
-				<div class="control-group">
-					<label for="bings" class="control-label" >Bings:</label>
-					<div class="controls">
-						<input name="bings" id="bings" class="input-mini" type="number" value="<?php echo $bings; ?>" title="Number of Bings to perform" required autofocus>
-						<button id="update" class="btn btn-danger" type="submit">update</button>
+				<h3>&ldquo;But, I Like <abbr title='Does it rhyme with "Moogle"?'>[Other Search Engine]</abbr> Better&hellip;&rdquo;</abbr></h3>
+				<p>That&rsquo;s where <em>bingtastic</em> comes in. Make sure you are logged in to Facebook, then use the fields provided to automate your bing queries.</p>
+				<p>We&rsquo;ll generate as many <?php echo $minwords; ?>-<?php echo $maxwords; ?> word phrases as you need, then open up a new tab next door to help you get those precious, precious points.</p>
+
+				<h3>Search Daily</h3>
+				<p>Earn from 15 to more than 30 points every day and <a href="http://www.bing.com/rewards/redeem/all">redeem them for one of many great offers</a>, or even donate them to a charity.</p>
+			</div>
+
+			<form id="automate" action="/#automate" method="get" class="form-horizontal span5 offset1 row">
+				<h2 class="heading">Automate Searches</h2>
+				<fieldset id="form-controls">
+					<div class="control-group">
+						<label for="bings" class="control-label" >Bings:</label>
+						<div class="controls">
+							<input name="bings" id="bings" class="input-mini" type="number" data-default="<?php echo $bings; ?>" value="<?php echo $bings; ?>" title="Number of Bings to perform" data-require-redraw="true" required autofocus>
+						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label for="min" class="control-label" >Minimum Delay:</label>
-					<div class="controls">
-						<input name="min" id="min" class="input-mini" type="number" value="<?php echo $min; ?>" title="minimum delay" required>
+
+					<p class="heading toggle-control">Change intervals</p>
+					<div id="intervals" class="toggle">
+						<div class="control-group">
+							<label for="min" class="control-label" >Minimum Delay:</label>
+							<div class="controls">
+								<input name="min" id="min" class="input-mini" type="number" data-default="<?php echo $min; ?>" value="<?php echo $min; ?>" title="minimum delay" required>
+							</div>
+						</div>
+						<div class="control-group">
+							<label for="max" class="control-label" >Maximum Delay:</label>
+							<div class="controls">
+								<input name="max" id="max" class="input-mini" type="number" data-default="<?php echo $max; ?>" value="<?php echo $max; ?>" title="maximum delay" required>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label for="max" class="control-label" >Maximum Delay:</label>
+				</fieldset>
+
+				<fieldset id="modified" class="control-group">
 					<div class="controls">
-						<input name="max" id="max" class="input-mini" type="number" value="<?php echo $max; ?>" title="maximum delay" required>
+						<button id="update" class="btn btn-success btn-large" type="submit">update</button>
+						<button id="reset" class="btn btn-danger btn-large" type="reset">reset</button>
 					</div>
-				</div>
-				<div class="control-group">
+				</fieldset>
+
+				<fieldset id="standard" class="control-group">
 					<div class="controls">
 						<button id="bingMe" class="btn btn-large btn-primary" type="button">bing me!</button>
 						<button id="stop" class="btn btn-large" type="button">stop</button>
 					</div>
-				</div>
-			</fieldset>
-
-			<div id="timer" >
-				<div id="statusbar" class="ui-progressbar"><div class="progress-label"></div></div>
-			</div>
-		</form>
+				</fieldset>
+			</form>
 
 		</section>
 
 		<section class="row">
-			<h3 id="phrase-heading" class="span12">Phraselist (<?php echo $bings; ?>)</h3>
-			<div id="phraselist" class="span12">
+			<h3 id="phrase-heading" class="span12 toggle-control">Phraselist (<?php echo $bings; ?>)</h3>
+			<div id="phraselist" class="span12 toggle">
 			<?php
 				$counter = 0;
 				while($counter < $bings){
--- scripts/main.js
+++ scripts/main.js
@@ -6,31 +6,58 @@
 (function($){
 	"use strict";
 
+	$(".toggle, #modified").hide();
 
-	var $phraselist = $("#phraselist").hide();
-
-	$("#phrase-heading").on("click", function(){
-		$(this).toggleClass("active");
-		$phraselist.toggleClass("active").slideToggle();
-		$("i.icon", $(this)).toggleClass("icon icon-expand-alt").toggleClass("icon icon-collapse-alt")
+	$(".toggle-control").on("click", function(){
+		$(this).toggleClass("active").next().toggleClass("active").slideToggle();
+		$("i.icon", $(this)).toggleClass("icon icon-expand-alt").toggleClass("icon icon-collapse-alt");
 	}).prepend($("<i>", {"class": "icon icon-expand-alt"}));
 
-	var $statusbar = $("#statusbar").progressbar({ max: 100 });
+
+	var status = false;
+	var $container = $("#automate");
+	var $timer = $("<div>", {id: "timer"}).appendTo($container).hide();
+	var $statusbar = $("<div>", {id: "statusbar"})
+		.append($("<div>", {"class": "progress-label"}))
+		.on("update", function(e, data){
+			var elapsed = 0;
+			var interval = 50;
+			$(".progress-label", $(this)).text((data.time / 1000) + " seconds until the next Bing. " + data.count + " remaining.");
+			status = window.setInterval(function(){
+				elapsed += interval;
+				$statusbar.progressbar({ value: Math.floor(((elapsed/(data.time*0.9)) * 100)) });
+			}, interval);
+		})
+		.on("reset", function(){
+			window.clearInterval(status);
+			$(this).progressbar({ value: 0 });
+		})
+		.appendTo($timer)
+		.progressbar({ max: 100 });
 
 	$(window).on("load", function(){
-		var timer;
-		var status;
-		var $timer = $("#timer").hide();
-		var $statusbar = $("#statusbar");
-		var $bings = $("a", $phraselist);
+		var timer = false;
+		var $bings = $("#phraselist a");
 		var $count = $("#bings");
 		var $minTime = $("#min");
 		var $maxTime = $("#max");
 
 		$("#bings, #min, #max").on("change", function(){
-			// console.log($(this).val());
-			if($(this).val() < 0 ){
-				$(this).val(0);
+			var $this = $(this);
+			var val = $this.val();
+			if(val < 0 ){
+				$this.val(0);
+			}
+
+			if($this.attr("data-require-redraw") === "true"){
+				var defaultval = $this.attr("data-default");
+				if(val === defaultval ){
+					$("#modified").hide();
+					$("#standard").show();
+				} else {
+					$("#modified").show();
+					$("#standard").hide();
+				}
 			}
 		});
 
@@ -45,19 +72,10 @@
 		function bingMe() {
 			// console.log("bingme");
 			var count = getInt($count.val()) - 1;
-			//refactor
-			// window.clearInterval(status);
-			// var progress = 0;
-			// status = window.setInterval(function(){
-			//  progress = progress + 50;
-			//  $status.trigger("update", progress/time);
-			// }, 50);
-
-			window.clearInterval(status);
-			$statusbar.progressbar({ value: 0 });
 			window.open($bings.eq(count).addClass("visited").attr("href"), "_newtab");
 
 			$count.val(count);
+			$statusbar.trigger("reset");
 
 			if(count === 0){
 				$timer.text("all done.");
@@ -67,29 +85,36 @@
 				var max = getInt($maxTime.val());
 				var seconds = ( getRandom(max - min)  + min );
 				var time = ( seconds * 1000 );
-				// console.log(time);
-				timer = 0;
-				$(".progress-label", $statusbar).text(seconds + " seconds until the next Bing...");
-				status = window.setInterval(function(){
-					timer += 10;
-					$statusbar.progressbar({ value: Math.floor((timer/(time*.85)) * 100) });
-				}, 10);
+
+				$statusbar.trigger("update", {"count": count, "time": time});
+
+				window.clearTimeout(timer);
 				timer = window.setTimeout(bingMe, time);
 			}
 		}
 
+		$("#reset").on("click", function(){
+			$("#modified").hide();
+			$("#standard").show();
+		});
+
 		var $start = $("#bingMe").on("click", function(e){
 			e.preventDefault();
 			$timer.show();
-			$(this).add('input[type="number"], #update').attr({disabled: "disabled"});
+			$("#form-controls").hide();
+			$(this).add('#automate input[type="number"], #modified button').attr({disabled: "disabled"});
 			bingMe();
 		});
 
 		var $stop = $("#stop").on("click", function(e){
 			e.preventDefault();
 			window.clearTimeout(timer);
+			$statusbar.trigger("reset");
 			$timer.hide();
-			$start.add('input[disabled], #update').removeAttr('disabled');
+			$("#form-controls").show();
+			$start.add('#automate input[disabled], #modified button').removeAttr('disabled');
 		});
+
+
 	});
 })(jQuery);
