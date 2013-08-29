5d2465d99d3cd7ffb57a260df5760161f64706e6
Paul Gueller <paul.gueller@7summitsagency.com>
2013-08-29 10:02:45 -0500

    combine landing page and API into single-page site

--- index.php
+++ index.php
@@ -14,9 +14,23 @@ $bings = (isset($_GET["bings"]) && $_GET["bings"] > 0) ? $_GET["bings"] : 30;
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head lang="en">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="robots" content="noindex, nofollow">
+
 		<title>it's bingtastic!</title>
 		<meta name="description" content="">
-		<?php include("includes/head.php"); ?>
+
+		<link rel="author" href="/humans.txt">
+		<link rel="sitemap" href="/sitemap.xml">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/icons/favicon.png">
+		<link rel="apple-touch-icon" href="/icons/57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/icons/72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/icons/114.png">
+		<link rel="stylesheet" href="/styles/main.css">
 	</head>
 	<body class="container">
 		<div id="fb-root"></div>
@@ -100,7 +114,106 @@ $bings = (isset($_GET["bings"]) && $_GET["bings"] > 0) ? $_GET["bings"] : 30;
 			</div>
 		</section>
 
-		<?php include("includes/footer.php"); ?>
+		<section class="row">
+			<div class="span12">
+				<h2><abbr title="Application Programming Interface">api</abbr></h2>
+				<p>Lorem ipsum.</p>
+				<div class="hgroup row">
+					<h3 class="span2">request types</h3>
+					<div class="hidden-phone">
+					<p class="span1">parameters</p>
+					<p class="span2">accepts</p>
+					<p class="span3">description</p>
+					<p class="span1">returns</p>
+					<p class="span3">example</p>
+					</div>
+				</div>
+
+
+				<dl class="api row">
+
+					<dt class="span2">categories</dt>
+					<dd class="row span10">
+						<div class="row">
+							<p class="span1"><i>n/a</i></p>
+							<p class="span2"><i>n/a</i></p>
+							<p class="span3">retrieve list of available categories</p>
+							<p class="span1"><code>array</code></p>
+							<p class="span3"><a href="/api/?request=categories" target="_blank">/api/?request=categories</a></p>
+						</div>
+					</dd>
+
+					<dt class="span2">word</dt>
+					<dd class="row span10">
+						<div class="row">
+							<p class="span1"><i>n/a</i></p>
+							<p class="span2"><i>n/a</i></p>
+							<p class="span3">retrieve list of available categories</p>
+							<p class="span1"><code>array</code></p>
+							<p class="span3"><a href="/api/?request=words" target="_blank">/api/?request=words</a></p>
+						</div>
+					</dd>
+
+					<dt class="span2"><em>default</em></dt>
+					<dd class="row span10">
+						<div class="row">
+							<div class="span1"><i>null</i></div>
+							<div class="span2"><i>n/a</i></div>
+							<div class="span3">...</div>
+							<div class="span1"><code>object</code> </div>
+							<div class="span3"><a href="/api/" target="_blank">/api/</a></div>
+						</div>
+
+						<div class="row">
+							<div class="span1">bings</div>
+							<div class="span2">integer</div>
+							<div class="span3">default: 30</div>
+							<div class="span1"><code>object</code> </div>
+							<div class="span3"><a href="/api/?bings=60" target="_blank">/api/?bings=60</a></div>
+						</div>
+
+						<div class="row">
+							<div class="span1">minwords</div>
+							<div class="span2">integer</div>
+							<div class="span3">default: 2</div>
+							<div class="span1"><code>object</code> </div>
+							<div class="span3"><a href="/api/?minwords=1" target="_blank">/api/?minwords=1</a></div>
+						</div>
+
+						<div class="row">
+							<div class="span1">maxwords</div>
+							<div class="span2">integer</div>
+							<div class="span3">default: 4</div>
+							<div class="span1"><code>object</code> </div>
+							<div class="span3"><a href="/api/?maxwords=6" target="_blank">/api/?maxwords=6</a></div>
+						</div>
+
+						<div class="row">
+							<div class="span1">categories</div>
+							<div class="span2">comma-separated-list</div>
+							<div class="span3">default: all</div>
+							<div class="span1"><code>object</code> </div>
+							<div class="span3"><a href="/api/?categories=images,videos" target="_blank">/api/?categories=images,videos</a></div>
+						</div>
+					</dd>
+				</ul>
+			</div>
+		</section>
+
+		<footer class="row">
+			<p class="span12 text-right"><small><a href="http://creativecommons.org/licenses/by-nc-sa/3.0" class="pull-right"><img src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" alt="Attribution-NonCommercial-ShareAlike" title="CC BY-NC-SA"></a> Looking to develop your own Bing rewards app? Feel free to use my <a href="api.php">API</a>.</small></p>
+		</footer>
+
+		<script src="//code.jquery.com/jquery-2.0.3.min.js"></script>
+		<script>window.jQuery || document.write('<script src="packages/jquery/jquery.min.js"><\/script>')</script>
+
+		<!--
+		<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+		<script>window.jQuery || document.write('<script src="scripts/jquery-validate.min.js"><\/script>')</script>
+		-->
+
+		<script src="scripts/main.min.js"></script>
+
 	</body>
 </html>
 
