eb2a6089cff8c3589af18da3c7414f275a009da4
Paul Gueller <paul.gueller@gmail.com>
2015-04-08 13:15:48 -0500

    update packages to latest, load 500px from bower, build with npm install

--- dev/scripts/source/social.js
+++ dev/scripts/source/social.js
@@ -69,13 +69,3 @@ window.fbAsyncInit = function() {
 	js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=" + app.id;
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-// Google Analytics
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-1575455-10', 'auto');
-ga('send', 'pageview');
--- web/index.php
+++ web/index.php
@@ -191,6 +191,15 @@ function version($urlString, $release = '4.1.13'){
 		</div>
 
 		<script src="<?php version('scripts/main.min.js'); ?>"></script>
+		<script>
+			// Google Analytics
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+			ga('create', 'UA-1575455-10', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</body>
 </html>
